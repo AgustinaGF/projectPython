2343adfa31c34e2815b687ff63ba2726c7ea21ff
Agustina Gomez Fernandez <agustinagomez385@gmail.com>
2022-07-02 01:00:45 -0300

    images

--- django_base/urls.py
+++ django_base/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django_base.views import index, login_view, logout_view, register_view
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', index, name='index'),
@@ -26,4 +29,4 @@ urlpatterns = [
     path('login/', login_view, name ='login'),
     path('logout/', logout_view, name ='logout'),
     path('register/', register_view, name ='register'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
+from distutils.command.upload import upload
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class User_profile(models.Model):
-    user= models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
     phone = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='profile_image')
